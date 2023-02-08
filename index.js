bf943f7db83f7b238c10b66ea38d21fa5d31d6cf
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-02-08 11:12:48 +0100

    changes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Importeer express uit de node_modules map
 import express from 'express'
 
+const url = 'https://whois.fdnd.nl/api/v1/member/fatihg61'
+const data = await fetch(url).then((response) ⇒ response.json())
+
+// console.log(data.member.name)
+
+
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -14,7 +20,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
